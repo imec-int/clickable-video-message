d263c31cd3d5753e3de9c505646543978c5feabf
Sam Decrock <sam.decrock@gmail.com>
2014-06-30 12:54:50 +0200

    working on iphone/ipod too

--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -1,6 +1,6 @@
 var App = function (options){
 
-	var handTime = 0.5;
+	var handTime = 5.5;
 
 	var $video = $("video");
 	var video = $video[0];
@@ -9,20 +9,20 @@ var App = function (options){
 		console.log("init");
 		initHandlers();
 		initOverlay();
+
+		FastClick.attach(document.body);
 	};
 
 	var initHandlers = function () {
 		$video.bind('timeupdate', onVideoTimeupdate);
 		$video.bind('ended', onVideoEnded);
-		$video.bind('webkitendfullscreen', onVideoEnded);
+		$video.bind('webkitendfullscreen', onVideoEndFullscreen);
 
 		$('.play').click(onPlayClicked);
 
 		$('.yesimage').click(onVideoYesClick);
 		$('.choice .yes').click(onChoiceYesClick);
 		$('.choice .no').click(onChoiceNoClick);
-
-		$video.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', onVideoTransitionEnd);
 	};
 
 	var initOverlay = function () {
@@ -35,15 +35,9 @@ var App = function (options){
 			if(videoWidth>maxvideoWidth) videoWidth = maxvideoWidth;
 			var videoheight = videoWidth/videoratio;
 
-
-
-			$('video').outerHeight( videoheight );
-			$('video').outerWidth( videoWidth );
-
-			console.log(videoWidth);
-
-			$('.overlay').outerHeight( videoheight );
-			$('.overlay').outerWidth( videoWidth );
+			// set dimensions video and overlay:
+			$('video, .overlay').outerHeight( videoheight );
+			$('video, .overlay').outerWidth( videoWidth );
 
 			// center video and overlay:
 			var x = ($(window).width() - videoWidth)/2;
@@ -57,6 +51,10 @@ var App = function (options){
 
 			// yesimage must always be square:
 			$('.yesimage').height(  $('.yesimage').width() );
+
+
+			// show content (if not shown):
+			$('.content').addClass('show');
 		});
 	};
 
@@ -68,16 +66,13 @@ var App = function (options){
 
 	var onVideoTimeupdate = function (event) {
 		if(video.paused) return;
-
-		$('.play').hide();
+		$('.play').hide(); //hide play icon
 
 		var time = event.target.currentTime;
 		if(time > handTime){
-			$('.choice').addClass('show');
-			$('.yesimage').addClass('show');
+			showChoise();
 		}else{
-			$('.choice').removeClass('show');
-			$('.yesimage').removeClass('show');
+			hideChoice();
 		}
 	};
 
@@ -85,8 +80,18 @@ var App = function (options){
 
 	};
 
-	var onVideoTransitionEnd = function (event) {
+	var onVideoEndFullscreen = function (event) {
+		// iPhone's a bitch, we have to remove the video for it to be clickable (replace it with an end image)
+		if(isIphoneIpod()){
+			showChoise();
 
+			$('.endimage').addClass('show');
+			// remove video after it's hidden:
+			$('.endimage').one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () { // 'one' executes the function only once
+				video.pause(); // just to make sure, can only happen in debugging
+				$video.hide();
+			});
+		}
 	};
 
 	var onVideoYesClick = function (event) {
@@ -101,16 +106,52 @@ var App = function (options){
 		showToobadMessage();
 	};
 
+	var showChoise = function () {
+		$('.choice').addClass('show');
+		$('.yesimage').addClass('show');
+	};
+
+	var hideChoice = function (callback) {
+		$('.choice').removeClass('show');
+		$('.yesimage').removeClass('show');
+
+		$('.yesimage').one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function () { // 'one' executes the function only once
+			if(callback) callback();
+		});
+	};
+
 	var showThankyouMessage = function () {
 		video.pause();
+
 		$('.thankyou').addClass('show');
-		$('.choice').removeClass('show');
+		// wait till choice is gone:
+		hideChoice(function () {
+			// wait some more to add dramatic effect :-)
+			setTimeout(function () {
+				$('.thankyou p').addClass('show');
+			},400);
+		});
 	};
 
 	var showToobadMessage = function () {
 		video.pause();
+
 		$('.toobad').addClass('show');
-		$('.choice').removeClass('show');
+		// wait till choice is gone:
+		hideChoice(function () {
+			// wait some more to add dramatic effect :-)
+			setTimeout(function () {
+				$('.toobad p').addClass('show');
+			},400);
+		});
+	};
+
+	var isIphoneIpod = function () {
+		if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+			return true;
+		}else{
+			return false;
+		}
 	};
 
 	return {
